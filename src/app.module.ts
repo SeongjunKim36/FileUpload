067bc2ac36b2e99906e37368fe85501d7f8c962c
BearMett <agomdole@gmail.com>
2025-01-17 00:06:31 +0900

    🐛 데이터베이스 포트가 환경변수를 참조하지 않고 상수로 작성되어있음

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { FileModule } from './modules/files/files.module';
             useFactory: (configService: ConfigService) => ({
                 type: 'postgres',
                 host: configService.get('DB_HOST'),
-                port: 5432,
+                port: Number(configService.get('DB_PORT') ?? '5432'),
                 username: configService.get('DB_USERNAME'),
                 password: configService.get('DB_PASSWORD'),
                 database: configService.get('DB_DATABASE'),
